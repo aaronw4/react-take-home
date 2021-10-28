0835fb05888597c5ac0f2ee4d9c12b67ef8b7cd4
Aaron Williams <aaronrw4@yahoo.com>
2021-10-28 16:39:01 -0700

    Update

--- app/src/Campaigns.js
+++ app/src/Campaigns.js
@@ -1,13 +1,15 @@
 import React from "react";
 import link from './images/link.png';
-import arrow from './images.Combined-Shape.png';
+import arrow from './images/Combined-Shape.png';
+import play from './images/play.png';
 
 const Campaigns = (props) => {
     const campaigns = props.campaigns
 
     return (
         <div id='APP'>
-            <h5 className='campTitle'>PLUGS</h5>
+            {console.log(campaigns)}
+            <h5 className='campMainTitle'>PLUGS</h5>
             {campaigns.map(campaign => (
                 <div>
                     <div key={campaign.id} className='campCont'>
@@ -16,12 +18,14 @@ const Campaigns = (props) => {
                             alt={campaign.campaign_name}
                             className='campImg'
                         />
-                        <p className='campText'>{campaign.campaign_name}</p>
+                        <div className='campTitle'>
+                            <p className='campText'>{campaign.campaign_name}</p>
+                            <p className='campInstall'>{campaign.pay_per_install} per install</p>
+                        </div>
                     </div>
                     <div className='mediaCont'>
                         {campaign.medias.map(media => (
                             <div>
-                                {console.log(media)}
                                 <div>
                                     <img
                                         src={media.cover_photo_url}
@@ -29,11 +33,11 @@ const Campaigns = (props) => {
                                         className='mediaImg'
                                     />
                                 </div>
-                                <div>
-                                    <button>
+                                <div className='buttonCont'>
+                                    <button className='mediaButton'>
                                         <img src={link} alt=''/>
                                     </button>
-                                    <button>
+                                    <button className='mediaButton'>
                                         <img src={arrow} alt=''/>
                                     </button>
                                 </div>
